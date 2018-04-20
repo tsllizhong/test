a938d6a31d6ec303cad94215c2b2b4f9637406a5
silence <silence@DESKTOP-3V259HK>
2018-04-20 09:51:52 +0800

    这是第一次修改(修改了student类)

--- ssm_test/src/main/java/com/hr/entity/Student.java
+++ ssm_test/src/main/java/com/hr/entity/Student.java
@@ -1,5 +1,19 @@
 package com.hr.entity;
 
 public class Student {
-
+	private int sid;
+	private String sname;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
 }
