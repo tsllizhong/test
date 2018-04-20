34d3e284f343c08484c04a7cf8e6327fa2f5a81a
silence <silence@DESKTOP-3V259HK>
2018-04-20 09:49:41 +0800

    fdsfds

---
+++ ssm_test/src/main/java/com/hr/SsmTestApplication.java
@@ -0,0 +1,12 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmTestApplication.class, args);
+	}
+}
